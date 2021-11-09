b6552791391a824bf384182ac7f346731a0acdff
duc1807 <trickytreat20@gmail.com>
2021-11-09 15:29:01 +0700

    feat: using RegEx for validate

--- src/services/validationSchema.js
+++ src/services/validationSchema.js
@@ -1,10 +1,17 @@
 import * as yup from 'yup'
 
 export const addApartmentValidator = yup.object().shape({
-  createdAt: yup.string().required('Created at is required'),
+  createdAt: yup
+    .string()
+    .required('Created at is required')
+    .matches(
+      /(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-([12]\d{3})/,
+      'Created time must be in DD-MM-YYYY format'
+    ),
   bedroom: yup
     .number()
     .min(1, ({ min }) => `Bedroom number must be at least ${min}`)
+    .max(99, ({ max }) => `Bedroom number can not be more than ${max}`)
     .typeError('Number only')
     .required('Bedroom is required'),
   rentPrice: yup
